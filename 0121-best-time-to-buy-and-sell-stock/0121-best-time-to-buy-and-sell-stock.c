a358d4b1d7abcc9fbe8456aac453aae5f788e19c
Tulasi H <tulsimini123@gmail.com>
2026-07-18 12:16:29 +0530

    Time: 0 ms (100%), Space: 16.2 MB (14.55%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -1,20 +1,17 @@
-int maxProfit(int* prices, int pricesSize) {
+int maxProfit(int *prices, int pricesSize) {
     int minPrice = prices[0];
     int maxProfit = 0;
 
-    for (int i = 1; i < pricesSize; i++) {
-
+    for (int i = 0; i < pricesSize; i++) {
         if (prices[i] < minPrice) {
             minPrice = prices[i];
         } else {
-
-            int profit = prices[i] - minPrice;
-
-            if (profit > maxProfit) {
-                maxProfit = profit;
+            int profit =  prices[i]- minPrice;
+            if(profit > maxProfit) {
+             maxProfit = profit;
             }
         }
+        
     }
-
     return maxProfit;
 }
