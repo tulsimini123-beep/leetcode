cd17aeb3f84ea29bd9233518d239d50460069b0b
Tulasi H <tulsimini123@gmail.com>
2026-07-23 12:07:59 +0530

    Time: 1 ms (24.82%), Space: 15.3 MB (87.33%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.c
@@ -0,0 +1,16 @@
+int maxSubArray(int* nums, int numsSize) {
+    int currentSum = nums[0];
+    int maxSum = nums[0];
+
+    for (int i = 1; i < numsSize; i++) {
+        if (currentSum < 0)
+            currentSum = nums[i];
+        else
+            currentSum += nums[i];
+
+        if (currentSum > maxSum)
+            maxSum = currentSum;
+    }
+
+    return maxSum;
+}
